0cc56b75a123e17f8d4ff8baefaf528b08d85157
LidiaGodo <lilula739@gmail.com>
2021-10-03 22:34:52 +0300

    added products section (markup, styles)

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import './img/advantages/01.svg';
 import './img/advantages/02.svg';
 import './img/advantages/03.svg';
 import './img/advantages/04.svg';
+import './img/products/prod01.jpg';
+import './img/products/prod02.jpg';
+import './img/products/prod03.jpg';
+import './img/products/prod04.jpg';
+import './img/products/prod05.jpg';
+import './img/products/prod06.jpg';
+import './img/products/prod07.jpg';
+import './img/products/prod08.jpg';
 
 import './styles/main.scss';
 
